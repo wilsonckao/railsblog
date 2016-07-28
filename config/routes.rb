b91156d895ec1c6a3113beaad7baf03fc2222957
Wilson Kao <wilsonckao@gmail.com>
2016-07-28 16:56:34 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
+
+  resources :articles do
+  resources :comments do
+
+  end
+  end
+end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
